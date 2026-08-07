acc30546bceaa8dad70e2b4bd930471e75efaae5
GIGACODE <gigacode@sber.com>
2026-08-07 19:40:05 +0300

    feat: increase task list width by another 5cm (1330px)
    Co-authored-by: GigaCode Assistant <gigacode@sberbank.ru>

--- task-api/src/components/App.tsx
+++ task-api/src/components/App.tsx
@@ -220,7 +220,7 @@ export function App() {
             />
           </div>
 
-          <div style={{ marginTop: '64px', marginRight: '15px', maxWidth: '1265px' }}>
+          <div style={{ marginTop: '64px', marginRight: '15px', maxWidth: '1330px' }}>
             <TaskList
               tasks={filteredTasks}
               onOpenDetails={(task) => setSelectedTask(task)}
